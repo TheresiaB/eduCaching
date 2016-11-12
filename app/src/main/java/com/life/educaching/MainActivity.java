797de43417ffdbaa80b5858de93d1045653726e9
karolinkliem <s0555039@htw-berlin.de>
2016-11-12 17:07:32 +0100

    projectKaro: Hausfarben und Schriften eingebunden, neues Layout erstellt

--- app/src/main/java/com/life/educaching/MainActivity.java
+++ app/src/main/java/com/life/educaching/MainActivity.java
@@ -1,17 +1,13 @@
 package com.life.educaching;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import static android.R.attr.text;
+public class MainActivity  {
 
-public class MainActivity extends AppCompatActivity {
-
-
-}
+    }
