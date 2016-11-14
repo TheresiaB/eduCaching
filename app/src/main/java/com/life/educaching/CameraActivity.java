067505fac1a34fd14858a39962ecaa4ae4c25abe
Praskovia Root <s0554193@htw-berlin.de>
2016-11-14 15:34:25 +0100

    Camera Methode korrigiert

--- app/src/main/java/com/life/educaching/CameraActivity.java
+++ app/src/main/java/com/life/educaching/CameraActivity.java
@@ -5,6 +5,7 @@ import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.VideoView;
 
 public class CameraActivity extends AppCompatActivity {
@@ -17,9 +18,7 @@ public class CameraActivity extends AppCompatActivity {
 
     static final int REQUEST_VIDEO_CAPTURE = 1;
 
-
-
-    private void dispatchTakeVideoIntent() {
+    public void dispatchTakeVideoIntent(View view) {
         Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
         if (takeVideoIntent.resolveActivity(getPackageManager()) != null) {
             startActivityForResult(takeVideoIntent, REQUEST_VIDEO_CAPTURE);
@@ -29,7 +28,7 @@ public class CameraActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
         if (requestCode == REQUEST_VIDEO_CAPTURE && resultCode == RESULT_OK) {
             Uri videoUri = intent.getData();
-            VideoView mVideoView = (VideoView) findViewById(R.id.mVideoView);
+            VideoView mVideoView = new VideoView(this);
             mVideoView.setVideoURI(videoUri);
         }
     }
