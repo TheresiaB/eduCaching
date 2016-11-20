cbfd3174215524ca29352bf5a04e25d42e3ad6ce
karolinkliem <s0555039@htw-berlin.de>
2016-11-20 19:04:58 +0100

    projektKaro:2 new drawable xml files, layout_bild modified

--- app/src/main/java/com/life/educaching/MainActivity.java
+++ app/src/main/java/com/life/educaching/MainActivity.java
@@ -3,13 +3,7 @@ package com.life.educaching;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity{
@@ -21,11 +15,8 @@ public class MainActivity extends AppCompatActivity{
         setContentView(R.layout.activity_main);
         Typeface myTypeface = Typeface.createFromAsset(getAssets(), "Signika-Regular.ttf");
         TextView myTextview = (TextView) findViewById(R.id.text_head);
+        myTextview.setTypeface(myTypeface);
     }
-public void openMap(View view)
-{
-    Intent intent = new Intent(this, MapsActivity.class);
-    startActivity(intent);
-}
+
 
 }
