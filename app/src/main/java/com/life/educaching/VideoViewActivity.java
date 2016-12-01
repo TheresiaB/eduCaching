e0f8c1652dc359f38a9ce65895d3df8e2a9cbc9f
Theresia <resi.bruns@gmx.de>
2016-12-01 11:25:55 +0100

    projectTheresia: Video abspielbar gemacht und angepasst

--- app/src/main/java/com/life/educaching/VideoViewActivity.java
+++ app/src/main/java/com/life/educaching/VideoViewActivity.java
@@ -2,51 +2,63 @@ package com.life.educaching;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.PixelFormat;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 import android.widget.VideoView;
 
 public class VideoViewActivity extends AppCompatActivity {
-    Button clk;
-    VideoView videov;
+    Button buttonVideoPlay;
+    VideoView myVideoView;
     Button buttonNext;
     Button buttonBack;
+    Button buttonVideoPause;
+    Button buttonVideoStop;
+    VideoView mVideoView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_information_video);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setContentView(R.layout.activity_video);
+        buttonVideoPlay = (Button) findViewById(R.id.video_play_button);
+        buttonVideoPause = (Button) findViewById(R.id.video_pause_button);
+        buttonVideoStop = (Button) findViewById(R.id.video_begin_button);
 
-        clk = (Button) findViewById(R.id.button);
-        videov = (VideoView) findViewById(R.id.videoView);
-        VideoOnClickListener videoOnClickListener = new VideoOnClickListener();
-        clk.setOnClickListener(videoOnClickListener);
+        myVideoView = (VideoView) findViewById(R.id.videoView);
+        String uriPath = "android.resource://" + getPackageName() + "/" + R.raw.boss;
+        Uri uri2 = Uri.parse(uriPath);
+        mVideoView2 = (VideoView) findViewById(R.id.videoView);
+        mVideoView2.setVideoURI(uri2);
+        mVideoView2.requestFocus();
 
-        setContentView(R.layout.activity_information_video);
+        buttonVideoPlay.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mVideoView2.start();
+            }
+        });
+
+        buttonVideoPause.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mVideoView2.pause();
+            }
+        });
+
+        buttonVideoStop.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mVideoView2.stopPlayback();
+            }
+        });
+
+
         addListenerOnButton();
-    }
 
-    public class VideoOnClickListener implements View.OnClickListener {
-        @Override
-        public void onClick(View view) {
-            videoplay();
-        }
-    }
-
-    public void videoplay()
-    {
-        String videopath = "android.resource://"+getPackageName()+"/"+R.raw.boss;
-        Uri uri = Uri.parse(videopath);
-        videov.setVideoURI(uri);
-        videov.start();
     }
 
     public void addListenerOnButton() {
@@ -62,8 +74,9 @@ public class VideoViewActivity extends AppCompatActivity {
                 startActivity(new Intent(context, MainActivity.class));
             }
         });
-
     }
+
+
 
 
 
