85e8f5a7856a22f630db02ba09161674382ddd65
karolinkliem <s0555039@htw-berlin.de>
2016-12-04 18:12:29 +0100

    projectKaro: new layout activity_station_1_geschafft, new java Station1DoneActivity, include in Manifest

--- app/src/main/java/com/life/educaching/Station1DoneActivity.java
+++ app/src/main/java/com/life/educaching/Station1DoneActivity.java
@@ -3,7 +3,6 @@ package com.life.educaching;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Typeface;
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -11,14 +10,7 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-
-public class StationMapsActivity extends AppCompatActivity {
+public class Station1DoneActivity extends AppCompatActivity {
 
     Button buttonNext;
     Button buttonBack;
@@ -26,7 +18,7 @@ public class StationMapsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_stationskarte);
+        setContentView(R.layout.activity_station_1_geschafft);
         Typeface myTypeface = Typeface.createFromAsset(getAssets(), "OpenSans-Regular.ttf");
         TextView myTextview = (TextView) findViewById(R.id.text_head);
         myTextview.setTypeface(myTypeface);
@@ -43,18 +35,25 @@ public class StationMapsActivity extends AppCompatActivity {
         buttonBack = (Button) findViewById(R.id.button_back);
 
 
-        buttonNext.setOnClickListener(new View.OnClickListener() {
+        buttonBack.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View arg0) {
-                Toast.makeText(StationMapsActivity.this, "Button Clicked", Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(context, Station1DoneActivity.class));
+                Toast.makeText(Station1DoneActivity.this, "Button Clicked", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(context, StationMapsActivity.class));
             }
         });
+
+
     }
 
     public void openMap (View view) {
         Intent intent = new Intent (this, MapsActivity.class);
         startActivity(intent);
     }
+
+
+
+
+
 }
