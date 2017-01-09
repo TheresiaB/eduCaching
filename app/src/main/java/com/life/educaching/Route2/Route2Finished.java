e9e38a23bb7a0441a2c053a808160109c3d9935e
Praskovia Root <s0554193@htw-berlin.de>
2017-01-09 14:46:53 +0100

    Route2Finished

--- app/src/main/java/com/life/educaching/Route2/Route2Finished.java
+++ app/src/main/java/com/life/educaching/Route2/Route2Finished.java
@@ -1,9 +1,11 @@
-package com.life.educaching;
+package com.life.educaching.Route2;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Typeface;
+import android.net.Uri;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,23 +14,32 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.life.educaching.Model.DecideRouteActivity;
+import com.life.educaching.Model.MapMethods;
+import com.life.educaching.Model.TaskTextAudioActivity;
+import com.life.educaching.R;
 
-public class Route2_OverviewMapActivity extends AppCompatActivity implements OnMapReadyCallback{
+public class Route2Finished extends AppCompatActivity implements OnMapReadyCallback {
 
     GoogleMap mMap;
-    Button buttonNext;
     Button buttonBack;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_route1_overviewmap);
+        setContentView(R.layout.activity_route2_finished);
         Typeface myTypeface = Typeface.createFromAsset(getAssets(), "OpenSans-Regular.ttf");
         TextView myTextview = (TextView) findViewById(R.id.text_head);
         myTextview.setTypeface(myTypeface);
@@ -46,58 +57,48 @@ public class Route2_OverviewMapActivity extends AppCompatActivity implements OnM
         //in your OnCreate() method
         myAwesomeTextView.setText(DecideRouteActivity.whichRoute);
     }
+
     public void addListenerOnButton() {
 
         final Context context = this;
-        buttonNext = (Button) findViewById(R.id.button_next);
         buttonBack = (Button) findViewById(R.id.button_back);
 
-
-        buttonNext.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View arg0) {
-                Toast.makeText(Route2_OverviewMapActivity.this, "Button Clicked", Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(context, StationMapsActivity.class));
-            }
-        });
         buttonBack.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View arg0) {
-                Toast.makeText(Route2_OverviewMapActivity.this, "Button Clicked", Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(context, InformationVideoActivity.class));
+                Toast.makeText(Route2Finished.this, "Button Clicked", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(context, TaskTextAudioActivity.class));
             }
         });
 
 
     }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         // Ein Marker in der ersten Station hinzufügen und die Kamera bewegen
         LatLng friedrichstrasse = new LatLng(52.5137447, 13.389356700000008);
-        mMap.addMarker(new MarkerOptions().position(friedrichstrasse).title("Marker in der 1. Station")).showInfoWindow();
+        mMap.addMarker(new MarkerOptions().position(friedrichstrasse).title("Marker in der 1. Station").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))).showInfoWindow();
         //mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(moeckernbruecke, 14));
 
         LatLng reichstag = new LatLng(52.5185353, 13.37318849999997);
-        mMap.addMarker(new MarkerOptions().position(reichstag).title("Marker in der 2. Station")).showInfoWindow();
+        mMap.addMarker(new MarkerOptions().position(reichstag).title("Marker in der 2. Station").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))).showInfoWindow();
 
         LatLng schoenhauserstr = new LatLng(52.5263005, 13.407798899999989);
-        mMap.addMarker(new MarkerOptions().position(schoenhauserstr).title("Marker in der 4. Station")).showInfoWindow();
+        mMap.addMarker(new MarkerOptions().position(schoenhauserstr).title("Marker in der 4. Station").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))).showInfoWindow();
 
-        LatLng [] stationen = {friedrichstrasse, reichstag, schoenhauserstr};
+        LatLng[] stationen = {friedrichstrasse, reichstag, schoenhauserstr};
 
         LatLngBounds Route = MapMethods.calculateLatLngBounds(stationen);
         mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(Route, 50));
 
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.INTERNET}, 10);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.INTERNET}, 10);
 
             return;
-        }
-        else
-        {
+        } else {
             setCurrentLocation();
         }
 
@@ -113,9 +114,10 @@ public class Route2_OverviewMapActivity extends AppCompatActivity implements OnM
                 return;
         }
     }
+
     //die nächste Zeile wird als inkorrekt angezeigt, weil der Compiler denkt, dass wir die Überprüfung des Zugriffs nicht gemacht haben, bevor der Standort angezeigt wird. Die Abfrage der Berechtigung erfolge aber schon in der vorigen Methode
     public void setCurrentLocation() {
         mMap.setMyLocationEnabled(true);
     }
 
-}
+   }
