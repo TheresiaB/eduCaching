b9e9427c0fa947161c6af3bcde2ed5ed53feeb04
karolinkliem <s0555039@htw-berlin.de>
2017-01-12 20:46:55 +0100

    projectKaro: Layout erste Seite

---
+++ app/src/main/java/com/life/educaching/Startpage_group_register_Activity.java
@@ -0,0 +1,44 @@
+package com.life.educaching;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.SupportMapFragment;
+
+/**
+ * Created by karolin on 18.12.16.
+ */
+
+public class Startpage_group_register_Activity extends AppCompatActivity {
+
+    Button buttonNewGroup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_startpage_group_register);
+        addListenerOnButton();
+    }
+
+    public void addListenerOnButton() {
+
+        final Context context = this;
+        buttonNewGroup = (Button) findViewById(R.id.button_newgroup);
+
+        buttonNewGroup.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                Toast.makeText(Startpage_group_register_Activity.this, "Button Clicked", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(context, DecideRouteActivity.class));
+            }
+        });
+    }
+}
