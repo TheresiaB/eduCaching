7090ba94ea9dff0fad7d74a5c99362b71058adb6
Priscilla <s0548696@htw-berlin.de>
2017-01-23 17:05:33 +0100

    projectpris:activityevalutation erstellt

---
+++ app/src/main/java/com/life/educaching/Route1_EvaluationActivity.java
@@ -0,0 +1,13 @@
+package com.life.educaching;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Route1_EvaluationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_route1__evaluation);
+    }
+}
